229c6daf6601d97715fbd00ebcdf26be2fe685d7
AlinaGaz <alinagazizova576@gmail.com>
2022-12-26 07:14:15 +0300

    Добавьте файлы проекта.

---
+++ Laba4.2/Laba4.2.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+using namespace std;
+class Weapon
+{
+public:
+	string name;
+	int damage;
+	int weight;
+	Weapon(string Name, int Damage, int Weight) {
+		name = Name;
+		damage = Damage;
+		weight = Weight;
+	};
+	Weapon() {
+		name = "Default";
+		damage = 10;
+		weight = 10;
+	};
+	~Weapon();
+
+private:
+
+};
+int main() {
+	Weapon first = Weapon("Sword", 20, 20);
+	Weapon second = Weapon();
+	cout << "first" << first.name << " " << first.damage << " " << first.weight << endl;
+	cout << "second" << second.name << " " << second.damage << " " << second.weight << endl;
+}
